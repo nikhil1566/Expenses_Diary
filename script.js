2b9bc9a3f2d36e398ded45e7d94015f114b39623
Nikhil kumar singh <86455294+nikhil1566@users.noreply.github.com>
2023-07-04 11:38:11 +0530

    script.js

--- script.js
+++ script.js
@@ -6,19 +6,9 @@ const form = document.getElementById('form');
 const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
-// const dummyTransactions = [
-//   { id: 1, text: 'Flower', amount: -20 },
-//   { id: 2, text: 'Salary', amount: 300 },
-//   { id: 3, text: 'Book', amount: -10 },
-//   { id: 4, text: 'Camera', amount: 150 }
-// ];
+const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'));
 
-const localStorageTransactions = JSON.parse(
-  localStorage.getItem('transactions')
-);
-
-let transactions =
-  localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : [];
 
 // Add transaction
 function addTransaction(e) {
@@ -53,44 +43,33 @@ function generateID() {
 
 // Add transactions to DOM list
 function addTransactionDOM(transaction) {
-  // Get sign
   const sign = transaction.amount < 0 ? '-' : '+';
-
   const item = document.createElement('li');
-
-  // Add class based on value
   item.classList.add(transaction.amount < 0 ? 'minus' : 'plus');
 
   item.innerHTML = `
-    ${transaction.text} <span>${sign}${Math.abs(
-    transaction.amount
-  )}</span> <button class="delete-btn" onclick="removeTransaction(${
-    transaction.id
-  })">x</button>
+   ${transaction.text} <span>${sign}₹${Math.abs(transaction.amount)}</span> <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
   `;
 
   list.appendChild(item);
 }
 
-// Update the balance, income and expense
+// Update the balance, income, and expense
 function updateValues() {
   const amounts = transactions.map(transaction => transaction.amount);
 
   const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
-
   const income = amounts
     .filter(item => item > 0)
     .reduce((acc, item) => (acc += item), 0)
     .toFixed(2);
-
   const expense = (
-    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) *
-    -1
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1
   ).toFixed(2);
 
-  balance.innerText = `$${total}`;
-  money_plus.innerText = `$${income}`;
-  money_minus.innerText = `$${expense}`;
+  balance.innerText = `₹${total}`;
+  money_plus.innerText = `₹${income}`;
+  money_minus.innerText = `₹${expense}`;
 }
 
 // Remove transaction by ID
